cada2af1b5f4f7220256e7acb7ec8d87aeba2080
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-05-15 16:48:45 +0900

    Style: add styles for screens larger than mobile size

--- the-global/src/App.js
+++ the-global/src/App.js
@@ -1,11 +1,7 @@
-import GlobalStyle from "./styles/GlobalStyle";
+import React from "react";
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- the-global/src/styles/GlobalStyle.js
+++ the-global/src/styles/GlobalStyle.js
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset};
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: #EBEEF3;
   }
 
   li {
@@ -17,6 +20,25 @@ const GlobalStyle = createGlobalStyle`
 
   button {
     cursor: pointer;
+  }
+
+  @media (min-width: 479px) {
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      background-color: white;
+    }
+
+    #root {
+      width: 100vw;
+      max-width: 479px;
+      height: 100vh;
+      margin: auto;
+      background-color: #EBEEF3;
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
   }
 `;
 
